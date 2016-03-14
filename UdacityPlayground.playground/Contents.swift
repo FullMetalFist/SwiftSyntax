c14b212bb66209d2c196bd31ca66b0e5cd1da052
Michael Vilabrera <m.vilabrera@gmail.com>
2016-03-14 11:18:42 -0400

    initial commit

---
+++ UdacityPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+class LightSwitch {
+    var on: Bool = true
+    var dimmer: Double = 3.14159265359
+}
+
+var livingRoomSwitch = LightSwitch()
+livingRoomSwitch.on
